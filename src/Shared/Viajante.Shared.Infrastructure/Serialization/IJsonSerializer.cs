a7c65d40c0dd250a3641ff4b601a25c271a1ae2b
Vladimir Sinyavsky <sva.sorcery@gmail.com>
2022-01-04 22:40:10 +0300

    Shared.Infra : Add json serialization

---
+++ src/Shared/Viajante.Shared.Infrastructure/Serialization/IJsonSerializer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Viajante.Shared.Infrastructure.Serialization
+{
+    public interface IJsonSerializer
+    {
+        string Serialize<T>(T value);
+        T Deserialize<T>(string value);
+        object Deserialize(string value, Type type);
+    }
+}
