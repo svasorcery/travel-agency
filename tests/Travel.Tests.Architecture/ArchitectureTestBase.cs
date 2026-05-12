804b68f90fade8cac64782236905dfc44e59a12f
Vladimir Sinyavskiy <sva.sorcery@gmail.com>
2026-05-12 23:04:35 +0300

    test(arch): add Travel.Tests.Architecture project with ArchUnit base

---
+++ tests/Travel.Tests.Architecture/ArchitectureTestBase.cs
@@ -0,0 +1,22 @@
+using ArchUnitNET.Domain;
+using ArchUnitNET.Loader;
+using Travel.Shared.Abstractions;
+
+namespace Travel.Tests.Architecture;
+
+public static class ArchitectureTestBase
+{
+    private static readonly Lazy<global::ArchUnitNET.Domain.Architecture> Lazy = new(() =>
+        new ArchLoader()
+            .LoadAssemblies(
+                typeof(IModuleAssemblyMarker).Assembly,
+                typeof(Modules.Flights.Core.FlightsModuleMarker).Assembly,
+                typeof(Modules.Hotels.Core.HotelsModuleMarker).Assembly,
+                typeof(Modules.Rail.Core.RailModuleMarker).Assembly,
+                typeof(Modules.Trips.Core.TripsModuleMarker).Assembly,
+                typeof(Modules.Identity.Core.IdentityModuleMarker).Assembly
+            )
+            .Build());
+
+    public static global::ArchUnitNET.Domain.Architecture Architecture => Lazy.Value;
+}
