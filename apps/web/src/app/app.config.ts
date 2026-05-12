36bf45a23728744fd93076b69d362810e1038d8e
Vladimir Sinyavskiy <sva.sorcery@gmail.com>
2026-05-12 22:53:45 +0300

    feat(web): add Angular 21 shell with zoneless + standalone + SSR

---
+++ apps/web/src/app/app.config.ts
@@ -0,0 +1,12 @@
+import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { appRoutes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZonelessChangeDetection(),
+    provideRouter(appRoutes),
+    provideHttpClient(withFetch()),
+  ],
+};
